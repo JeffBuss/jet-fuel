cbfd3109813a2bf4eec30ed83d55316f6b5d2891
bekahlundy <lundyrebekah@gmail.com>
2017-03-17 11:25:54 -0600

    put back event default

--- public/app/app.js
+++ public/app/app.js
@@ -8,6 +8,7 @@ const folderList = $('.folder-list')
 let currentFolder = undefined
 
 folderBtn.on('click', (event) => {
+  event.preventDefault()
   let input = $('.folder-input').val()
   saveFolder(input)
   clearFolders()
